6f17bf9a6d1d94bc5ed7b43e0b86b21a05ae0c15
Tadas Petra <60107328+tadaspetra@users.noreply.github.com>
2020-11-07 08:16:51 -0600

    Revert "Update home.dart"
    This reverts commit cd34f751f2587472e1a77ac12a9c85522c9d8d8e.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,67 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Color twitterColor = Colors.grey;
-
-  Future<void> _launchTwitter() async {
-    const url = 'https://twitter.com/tadaspetra';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text("My name is Tadas Petra"),
-        const SizedBox(height: 20),
-        const SelectableText("My name is Tadas Petra"),
-        const SizedBox(height: 20),
-        MouseRegion(
-          onEnter: (_) {
-            twitterColor = Colors.white;
-            setState(() {});
-          },
-          onExit: (_) {
-            twitterColor = Colors.grey;
-            setState(() {});
-          },
-          child: Row(
-            children: [
-              IconButton(
-                iconSize: 100,
-                icon: SizedBox(
-                  width: 100,
-                  child: Image.asset(
-                    "assets/images/twitter.png",
-                    color: twitterColor,
-                  ),
-                ),
-                color: Colors.white,
-                onPressed: _launchTwitter,
-              ),
-              Text(
-                "Follow Me",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: twitterColor,
-                ),
-              ),
-            ],
-          ),
-        ),
-        SizedBox()
-      ],
-    );
+    return const Text("My name is Tadas Petra");
   }
 }
