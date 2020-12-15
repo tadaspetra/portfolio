7ec2f969d5f0c4d72a70bac7f1918f962f303aa1
Tadas Petra <60107328+tadaspetra@users.noreply.github.com>
2020-12-15 13:57:11 -0600

    main screen hover effect

--- lib/pages/consulting.dart
+++ lib/pages/consulting.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ConsultingPage extends StatelessWidget {
+  static const routeName = '/consulting';
   @override
   Widget build(BuildContext context) {
     return const Text("Consulting");
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../hover_effect.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -8,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color twitterColor = Colors.grey;
+  Animation<FractionalOffset> animation;
 
   Future<void> _launchTwitter() async {
     const url = "https://twitter.com/tadaspetra";
@@ -20,44 +23,77 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SelectableText(
-            "       This is a work in progress,\n\n        but if you want updates:\n"),
-        MouseRegion(
-          onEnter: (_) {
-            twitterColor = Colors.white;
-            setState(() {});
-          },
-          onExit: (_) {
-            twitterColor = Colors.grey;
-            setState(() {});
-          },
-          child: Row(
-            children: [
-              IconButton(
-                iconSize: 30,
-                icon: SizedBox(
-                  width: 100,
-                  child: Image.asset(
-                    "assets/images/twitter.png",
-                    color: twitterColor,
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.2,
+      height: MediaQuery.of(context).size.height / 1.5,
+      child: HoverCard(
+        builder: (context, hovering) {
+          return Container(
+            color: Color(0xFF3F3F3F),
+            child: Center(
+              // ignore: sort_child_properties_last
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/tplogo.png',
+                    color: Colors.white,
+                    width: 120,
                   ),
-                ),
-                color: Colors.white,
-                onPressed: _launchTwitter,
+                  const SelectableText(
+                      "       This is a work in progress,\n\n        but if you want updates:\n"),
+                  MouseRegion(
+                    onEnter: (_) {
+                      twitterColor = Colors.white;
+                      setState(() {});
+                    },
+                    onExit: (_) {
+                      twitterColor = Colors.grey;
+                      setState(() {});
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          iconSize: 30,
+                          icon: SizedBox(
+                            width: 100,
+                            child: Image.asset(
+                              "assets/images/twitter.png",
+                              color: twitterColor,
+                            ),
+                          ),
+                          color: Colors.white,
+                          onPressed: _launchTwitter,
+                        ),
+                        Text(
+                          "Follow Me",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            color: twitterColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                "Follow Me",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  color: twitterColor,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+            ),
+          );
+        },
+        animation: animation,
+        animationCallback: (tween) {
+          animation = tween;
+        },
+        depth: 10,
+        depthColor: Colors.grey[500],
+        onTap: () => print('Hello, World!'),
+        shadow: BoxShadow(
+            color: Color(0xFF80E2FD),
+            blurRadius: 30,
+            spreadRadius: -20,
+            offset: Offset(0, 40)),
+      ),
     );
   }
 }
