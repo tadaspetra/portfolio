4c79db120f48dc63f581054ab57debc81c4a0788
Tadas Petra <60107328+tadaspetra@users.noreply.github.com>
2021-02-14 20:15:52 -0600

    basic layout

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/customtheme.dart';
 import 'package:portfolio/pages/home.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: CustomTheme().buildTheme(),
       initialRoute: "/",
       routes: {
         "/": (context) => HomePage(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,7 +4,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hey"),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                "images/tp_logo.png",
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text("My name is Tadas Petra"),
+              Image.asset(
+                "images/profilepicture.png",
+                width: 200,
+                height: 200,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Image.asset(
+                "images/twitter_logo.png",
+                width: 50,
+                height: 50,
+              ),
+              Image.asset(
+                "images/youtube_logo.png",
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Image.asset(
+                "images/github_logo.png",
+                width: 40,
+                height: 40,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
